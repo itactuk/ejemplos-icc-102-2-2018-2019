3c739b85a23b7b519e2e2006ad5dbd839c34c519
i.tactuk <i.tactuk@ce.pucmm.edu.do>
2019-01-28 17:59:08 -0400

    agregue ejemplo de for y while

---
+++ Ej 3 for.py
@@ -0,0 +1,16 @@
+# Programa que imprima los números del 1 al 100
+
+arreglo = [1, 2, 3,4,5, 6]
+
+for i in range(1,101): # se itera sobre un arreglo
+    print(i)
+    print("a")
+print("b")
+
+for letra in "Hola":
+    print(letra)
+
+i = 0
+while i < 101:
+    print(i)
+    i = i + 1 # incremento

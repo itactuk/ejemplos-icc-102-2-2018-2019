0111498835ba510fa093fbed703ffd27cd36abf5
i.tactuk <i.tactuk@ce.pucmm.edu.do>
2019-01-28 16:53:20 -0400

    agregue ejemplos de declaracion de variables

---
+++ variables.py
@@ -0,0 +1,32 @@
+edad = 1 # int
+cantidad_personas = 10 # int
+precio_manzana = 10.2 # float
+nombre = "Chantal" # string
+pais = "'Republica Dominican'" # String
+mensaje = "Hola"
+tengo_hambre = True # bool
+sueno = False # bool
+
+matriculas = ["2015-1664", "2019-1665", "2014-2222"] # arreglo de strings
+#posiciones 0,1,2,...
+alturas = [1,3,545,7,876,8]
+lista_supermercado = ['manzana', 'naranja', 'pera']
+
+diccionario_espanol={'espanol':'idioma hablado en paises hispanoparlantes',
+'lengua': 'parte del cuerpo'} #diccionario
+
+meses={1:'enero', 2:'febrero' } # diccionarios
+
+
+print(matriculas[1])
+
+
+
+
+
+
+
+
+
+
+
